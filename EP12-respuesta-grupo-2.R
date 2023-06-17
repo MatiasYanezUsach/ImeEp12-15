73e48fffa1e810c08aa7ad084003f60aea0669b4
Matias Yanez <matias.yanez@usach.cl>
2023-06-17 11:00:48 -0400

    correccion comentarios y ortografia

--- EP12-respuesta-grupo-2.R
+++ EP12-respuesta-grupo-2.R
@@ -1,24 +1,24 @@
 # Grupo 2
-# Integrantes: 
-# Matias Yañez Muñoz
+# Integrantes:
+# Matías Yáñez Muñoz
 # Pablo Villarreal Ortiz
 # David Valero Croma
 
-# Explortacion de las librerias
+# Importación de librerías
 
-if (!require ( dplyr )) {
-  install.packages ("dplyr" , dependencies=TRUE)
-  require (dplyr)
+if (!require(dplyr)) {
+  install.packages("dplyr", dependencies = TRUE)
+  require(dplyr)
 }
 
-if (!require ( ggpubr )) {
-  install.packages ("ggpubr" , dependencies=TRUE)
-  require (ggpubr)
+if (!require(ggpubr)) {
+  install.packages("ggpubr", dependencies = TRUE)
+  require(ggpubr)
 }
 
-if (!require ( tidyverse )) {
-  install.packages ("tidyverse" , dependencies=TRUE)
-  require (tidyverse)
+if (!require(tidyverse)) {
+  install.packages("tidyverse", dependencies = TRUE)
+  require(tidyverse)
 }
 
 # ------------------------------ ACTIVIDADES --------------------------------------
@@ -67,7 +67,7 @@ if (!require ( tidyverse )) {
 winter_nelly <- c(150.6948, 144.935, 321.2211, 153.4385, 122.8188, 171.2131, 157.9422, 219.7431, 204.1157)
 golden_bosc <- c(130.2934, 192.9736, 227.2896, 160.5874, 132.915, 118.7482, 149.1814, 236.0405, 191.3631, 133.928, 367.991, 144.3353)
 
-# Se formulan las siguientes hipotesis:
+# Se formulan las siguientes hipótesis:
 
 # H0: No hay diferencia significativa entre las medias de ambas variedades de peras en la semana 15 de crecimiento.
 # H1: Existe una diferencia significativa entre las medias de ambas variedades de peras en la semana 15 de crecimiento.
@@ -79,70 +79,91 @@ golden_bosc <- c(130.2934, 192.9736, 227.2896, 160.5874, 132.915, 118.7482, 149.
 
 alfa <- 0.05
 
-# Revisión de condiciones para prueba t de student de comparación de medias
-
-# Gráfico de dispersión e histograma.
+# Revisión de condiciones para prueba t de Student de comparación de medias
+# Se utiliza Shapiro para verificar si tiene una distribución normal
 
 print(shapiro.test(winter_nelly))
 print(shapiro.test(golden_bosc))
 
-g1 <- gghistogram(winter_nelly, color= "blue", fill = "blue", bins = 10, xlab = "Peso (Gramos)", ylab = "Peras (Unidad)")
-g2 <- gghistogram(golden_bosc, color= "red", fill = "red", bins = 10, xlab = "Peso (Gramos)", ylab = "Peras (Unidad)")
+# Si observamos los valores obtenidos para el p-valor, resultan menores que al nivel
+# De significancia previamente definido, por lo que no se distribuye de forma normal
+# lo que indica que no se cumple la normalidad para las variables observadas.
+
+# Se realizan los gráficos de dispersión e histograma correspondientes
+g1 <- gghistogram(winter_nelly, color = "blue", 
+                  fill = "blue", bins = 10, 
+                  xlab = "Peso (Gramos)", 
+                  ylab = "Peras (Unidad)")
+
+g2 <- gghistogram(golden_bosc, color = "red", 
+                  fill = "red", bins = 10, 
+                  xlab = "Peso (Gramos)",
+                  ylab = "Peras (Unidad)")
 
 print(g1)
 print(g2)
 
-# Comprobación similaridad a distribución normal mediante gráfico Q-Q.
-
-g1_q_q <- ggqqplot(data = winter_nelly,
-              color = "steelblue",
-              xlab = "Teórico",
-              ylab = "Muestra",
-              title = "Gráfico Q-Q muestra v/s distr. normal")
+# Se realizan los gráficos Q-Q para mostrar la distribución. 
+g1_q_q <- ggqqplot(data = winter_nelly, 
+                   color = "steelblue", 
+                   xlab = "Teórico", 
+                   ylab = "Muestra", 
+                   title = "Gráfico Q-Q muestra Winter Nelly vs. distribución normal")
 print(g1_q_q)
 
-g2_q_q <- ggqqplot(data = golden_bosc,
-              color = "steelblue",
-              xlab = "Teórico",
-              ylab = "Muestra",
-              title = "Gráfico Q-Q muestra v/s distr. normal")
+g2_q_q <- ggqqplot(data = golden_bosc, 
+                   color = "steelblue", 
+                   xlab = "Teórico",
+                   ylab = "Muestra", 
+                   title = "Gráfico Q-Q muestra Golden Bosc vs. distribución normal")
 print(g2_q_q)
+# Como podemos observar en ambos gráficos Q-Q, hay presencia marcada de valores atípicos.
 
-# Para ambos casos, con un alfa = 0.05 el valor de p en los test de Shapiro-Wilk
-# resulta menor, por lo que no se cumple la normalidad para las variables observadas.
-
-# Transformación logarítmica debido a la asimetría de los datos
-
+# Se realiza una transformación logarítmica debido a la asimetría de los datos
 log_winter_nelly <- log(winter_nelly)
 log_golden_bosc <- log(golden_bosc)
 
+# Se utiliza Shapiro para verificar si las transformaciones tienen una distribución normal
 print(shapiro.test(log_winter_nelly))
 print(shapiro.test(log_golden_bosc))
 
-log_g1 <- gghistogram(log_winter_nelly, color= "blue", fill = "blue", bins = 10, xlab = "Peso (Gramos)", ylab = "Peras (Unidad)")
-log_g2 <- gghistogram(log_golden_bosc, color= "red", fill = "red", bins = 10, xlab = "Peso (Gramos)", ylab = "Peras (Unidad)")
+# Ahora si observamos el p-valor, podemos observar que se cumple una distribución normal,
+# lo que indica que se cumple la normalidad para las variables observadas. 
+
+# Se realizan los gráficos de dispersión e histograma correspondientes
+log_g1 <- gghistogram(log_winter_nelly, 
+                      color = "blue", 
+                      fill = "blue",
+                      bins = 10, 
+                      xlab = "Peso (Gramos)", 
+                      ylab = "Peras (Unidad)")
+
+log_g2 <- gghistogram(log_golden_bosc, 
+                      color = "red", 
+                      fill = "red", 
+                      bins = 10, 
+                      xlab = "Peso (Gramos)", 
+                      ylab = "Peras (Unidad)")
 
 print(log_g1)
 print(log_g2)
 
-# Comprobación similaridad a distribución normal mediante gráfico Q-Q luego de la transformación.
-
+# Comprobación de similitud a distribución normal mediante gráfico Q-Q
 log_g1_q_q <- ggqqplot(data = log_winter_nelly,
-              color = "steelblue",
-              xlab = "Teórico",
-              ylab = "Muestra",
-              title = "Gráfico Q-Q muestra v/s distr. normal")
+                       color = "steelblue", 
+                       xlab = "Teórico", 
+                       ylab = "Muestra", 
+                       title = "Gráfico Q-Q muestra transformada Winter Nelly vs. distribución normal")
 print(log_g1_q_q)
 
-log_g2_q_q <- ggqqplot(data = log_golden_bosc,
-              color = "steelblue",
-              xlab = "Teórico",
-              ylab = "Muestra",
-              title = "Gráfico Q-Q muestra v/s distr. normal")
+log_g2_q_q <- ggqqplot(data = log_golden_bosc, 
+                       color = "steelblue",
+                       xlab = "Teórico",
+                       ylab = "Muestra",
+                       title = "Gráfico Q-Q muestra transformada Golden Bosc vs. distribución normal")
 print(log_g2_q_q)
 
-# Puede observarse que el valor p de la prueba de Shapiro-Wilk poseen un valor p mayor al alfa definido,
-# por lo que se valida normalidad luego de la transformación.
+#Ahora como podemos observar en ambos graficos Q-Q, no se observan valores atipicos.
 
 # Prueba t de Student con condiciones cumplidas
 
@@ -153,11 +174,14 @@ prueba <- t.test(x = log_winter_nelly,
                  mu = 0,
                  conf.level = 1 - alfa)
 
+#se imprime la prueba realizada
 print(prueba)
 
-# Para el valor p obtenido, p = 0.8895 mayor a alfa = 0.05, se rechaza la hipótesis alternativa a favor
-# de la hipótesis nula, por lo que se concluye con un 95% de confianza para una transformación de variables
-# que no hay diferencia significativa entre las medias de ambas variedades en la semana 15 de crecimiento.
+# Como el valor de p obtenido es 0.8895 y este es mayor al alfa (0.05), entonces se falla en rechazar 
+# la hipótesis nula en favor de la hipótesis alternativa. Por lo tanto, se concluye con un 95% de confianza 
+# que no hay evidencia suficiente para afirmar que existe una diferencia significativa entre los pesos de 
+# las variedades de Winter Nelly y Golden Bosc en la semana 15 de crecimiento. Por lo tanto, se concluye 
+# que en esta etapa de desarrollo, el peso de ambas variedades es similar.
 
 # 2. Analice la primera pregunta abordada en el ejercicio práctico 11, con los mismos datos, utilizando un
 # método robusto adecuado.
